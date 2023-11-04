b8bb10e07ec2842c69b2a0d3647c0cf3362d33ac
Alessandro Ciccia <alessandro.099@icloud.com>
2023-11-04 16:46:04 +0100

    tentativo build

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -77,10 +77,9 @@ export async function getStaticPaths() {
   const category = await fetchDataFromApi("/api/categories?populate=*");
   const paths = category?.data?.map((c) => ({
     params: {
-      slug: category?.data?.[0]?.attributes?.slug,
+      slug: c?.attributes?.slug,
     },
   }));
-  console.log(category?.data?.[0]?.attributes?.slug);
 
   return {
     paths,
