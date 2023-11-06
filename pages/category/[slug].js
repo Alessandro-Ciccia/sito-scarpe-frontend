2823ee5d9f5b709c4dd9b8cd24c2526922441180
Alessandro Ciccia <alessandro.099@icloud.com>
2023-11-06 16:38:37 +0100

    tentativo build 5

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -75,7 +75,8 @@ export default Category;
 
 export async function getStaticPaths() {
   const category = await fetchDataFromApi("/api/categories?populate=*");
-  const paths = category.data.map((c) => ({
+  console.log(category.data);
+  const paths = await category?.data?.map((c) => ({
     params: {
       slug: c.attributes.slug,
     },
